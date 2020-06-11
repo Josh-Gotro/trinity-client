f5368546c282bd498abba214bb9d9a60a05f0146
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-06-11 10:50:44 -0500

    react hook form

--- src/NewPLForm.js
+++ src/NewPLForm.js
@@ -3,11 +3,11 @@ import { currentVendors } from './services/Atom';
 // import { currentPriceLists } from './services/Atom';
 import { currentUser } from './services/Atom';
 import { useRecoilValue } from 'recoil';
+import { useForm } from "react-hook-form";
 
 
 function NewPLForm() {
-    const [vendorSelect, setVendorSelect] = useState("");
-    const [dateSelect, setDateSelect] = useState("");
+    const { register, handleSubmit, errors } = useForm();
 
     let vendors = useRecoilValue(currentVendors)
     let crrntUser = useRecoilValue(currentUser)
@@ -16,13 +16,16 @@ function NewPLForm() {
         // console.log(vendors)
         if (vendors.length > 0) {
             return vendors.filter(vendor => vendor.user_id === crrntUser.id)
-                .map(vendor => <option key={vendor.id} name="vendorSelect" value={vendor.id} > {vendor.name}</option>)
+                .map(vendor => <option key={vendor.id} value={vendor.id} > {vendor.name}</option>)
             }
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+    const onSubmit = (e, r ) => {
+        // e.preventDefault()
         alert(`Price List Created`)
+        console.log(e);
+        
+
         // fetch(`http://localhost:3001/vendors`, {
         //     method: "POST",
         //     headers: {
@@ -42,49 +45,52 @@ function NewPLForm() {
         //         localStorage.setItem("token", data.jwt);
         //         setVendors(prev => [...prev, data]);
         //     })
+        r.target.reset()
     }
 
     return (
         <div>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <br></br>
                 <h1>New Price List</h1>
                 <br></br>
                 <label>
-                    Select Vendor
-                    <select onChange={e => setVendorSelect(e.target.value)}>
+                    <select ref={register({ required: true })} name="vendor" >
+                        <option value="" > Select Vendor</option>
                         {vendorOptions()}
-                    </select><br></br><br></br>
+                    </select>
+                    {errors.vendor && <p>Please select vendor. </p>}<br></br><br></br>
                     Date:
-                    <input type="date" name="dateSelect" onChange={e => setDateSelect(e.target.value)}  /><br></br><br></br>
+                    <input type="date" name="date" ref={register({required: true})} />
+                    {errors.date && <p>Please select date. </p>}<br></br><br></br>
 
                     Item Name:
-                    <input type="text" name="name" />
+                    <input type="text" name="name1" ref={register} />
                     $
-                    <input type="number" name="name" />
+                    <input type="number" name="price1" ref={register} />
                     per
-                    <input type="text" name="name" /><br></br>
+                    <input type="text" name="size1" ref={register} /><br></br>
 
                     Item Name:
-                    <input type="text" name="name" />
+                    <input type="text" name="name2" ref={register}  />
                     $
-                    <input type="number" name="name" />
+                    <input type="number" name="price2" ref={register} />
                     per
-                    <input type="text" name="name" /><br></br>
+                    <input type="text" name="size2" ref={register} /><br></br>
 
                     Item Name:
-                    <input type="text" name="name" />
+                    <input type="text" name="name3" ref={register} />
                     $
-                    <input type="number" name="name" />
+                    <input type="number" name="price3" ref={register}  />
                     per
-                    <input type="text" name="name" /><br></br>
+                    <input type="text" name="size3" ref={register} /><br></br>
 
                     Item Name:
-                    <input type="text" name="name" />
+                    <input type="text" name="name4" ref={register}  />
                     $
-                    <input type="number" name="name" />
+                    <input type="number" name="price4" ref={register}  />
                     per
-                    <input type="text" name="name" /><br></br>
+                    <input type="text" name="size4" ref={register} /><br></br>
 
                 </label>
                 <input type="submit" value="Save Price List" />
