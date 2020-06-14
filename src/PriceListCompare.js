d5e4329487339c61dc95057521f0d3d9bdbb714f
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-06-14 15:59:35 -0500

    most recent date in compare

--- src/PriceListCompare.js
+++ src/PriceListCompare.js
@@ -1,35 +1,77 @@
-import React from 'react';
+import React, { useState, } from 'react';
 import { currentPriceLists } from './services/Atom';
 import { useRecoilValue } from 'recoil';
 import PriceList from './PriceList';
 
-function PriceListCompare(props){
+function PriceListCompare(props) {
     let cpl = useRecoilValue(currentPriceLists)
+    const [vendorUserPricelists, setVendorUserPricelists] = useState([]);
+    const [mostRecent, setMostRecent] = useState("");
+    const [compare, setCompare] = useState("");
     let usrId = props.userInfo.id
     let vndrId = props.currentVendor.id
+    let date = "0000-00-00"
+    let newestPL = {}
 
-// console.log(props)
-// console.log(usrId)
-// console.log(vndrId)
 
-const displayPL = () => {
-    // console.log(cpl)
+
+    // console.log(props)
     // console.log(usrId)
-    // console.log(props.currentVendor)
-    if (cpl !== undefined) { 
-        return cpl.filter(pl => pl.user_id === usrId && pl.vendor_id === vndrId)
-        .map(pl => {
-            // console.log(pl)
-            return <PriceList  key={pl.id} plInfo={pl}/>
-        })
-    }
-    // return null
-}
+    // console.log(vndrId)
 
+    const displayPL = () => {
+        // console.log(cpl)
+        // console.log(usrId)
+        // console.log(props.currentVendor)
+        if (cpl !== undefined) {
+            return cpl.filter(pl => pl.user_id === usrId && pl.vendor_id === vndrId)
+                .map(pl => {
+                    // console.log(pl)
+                    return <PriceList key={pl.id} plInfo={pl} />
+                })
+        }
+        // return null
+    }
+
+    const displayMostRecent = () => {
+        findMostRecent();
+        return <PriceList key={newestPL.id} plInfo={newestPL} />
+        // console.log(cpl)
+        // console.log(usrId)
+        // console.log(props.currentVendor)
+        // if (cpl !== undefined) {
+        //     return cpl.filter(pl => pl.user_id === usrId && pl.vendor_id === vndrId)
+        //         .map(pl => {
+        //             // console.log(pl)
+        //             return <PriceList key={pl.id} plInfo={pl} />
+        //         })
+        // }
+        // return null
+    }
+
+    const findMostRecent = () => {
+        if (cpl !== undefined) {
+            return cpl.filter(pl => pl.user_id === usrId && pl.vendor_id === vndrId)
+                .map(pl => {
+                    // console.log(pl)
+                    console.log(pl.date)
+                    console.log(date)
+                    if (pl.date > date) {
+                        date = pl.date
+                        newestPL = pl
+                    }
+                    console.log(date)
+                    console.log(newestPL)
+                    // setVendorUserPricelists(pl)
+                })
+        }
+        console.log(newestPL)
+    }
 
     return (
         <div>
-            {displayPL()}
+            {displayMostRecent()}
+            {/* {displayPL()} */}
         </div>
     );
 }
